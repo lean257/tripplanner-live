e1ea4afccc7d723cf11f5b953254b2d9d975ac75
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-08-15 15:36:52 -0400

    working add buttons

--- models/_db.js
+++ models/_db.js
@@ -1,5 +1,7 @@
 // set up the database in this file
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/tripplanner')
+const db = new Sequelize('postgres://localhost:5432/tripplanner', {
+  logging: false
+})
 
 module.exports = db
